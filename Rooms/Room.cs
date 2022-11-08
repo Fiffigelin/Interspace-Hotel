683bd7b7a0591f6ab1195e67cb38f63f9471d44f
Elin Nyman <elin.nyman87@gmail.com>
2022-11-08 11:22:38 +0100

    ToString() in Room.cs

--- Rooms/Room.cs
+++ Rooms/Room.cs
@@ -1,7 +1,5 @@
 class Room
 {
-    List<Room> oneBed = new List<Room>();
-
     public int ID { get; set; }
     public int Price { get; }
     public int Bed { get; set; }
@@ -9,13 +7,24 @@ class Room
     public string Type { get; }
     public int Guests { get; set; }
 
-    public Room(int price, int bed, double size, string type, int guests)
+    public Room (int price, int bed, double size, string type, int guests)
     {
         this.Price = price;
         this.Bed = bed;
         this.Size = size;
         this.Type = type;
         this.Guests = guests;
+    }
+
+    public override string ToString()
+    {
+        return 
+        @$"Roomid : {ID}
+        Price : {Price}
+        Beds : {Bed}
+        Roomsize : {Size} kvm
+        Roomtype : {Type}
+        Max amount guests : {Guests}";
     }
 
     public enum StarReview
