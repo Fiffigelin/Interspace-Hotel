90121f204220d849c8b711f92e32f636a25f9a6b
Tricksterone <johan.karlsson089@gmail.com>
2022-11-08 11:39:21 +0100

    methods

--- Program.cs
+++ Program.cs
@@ -5,23 +5,19 @@ internal class Program
     private static void Main(string[] args)
     {
         List<Room> fakeRooms = new List<Room>();
-
-        Room oneBed = new(840, 1, 12, "oneBed", 1);
-        Room twoBed = new(1000, 1, 24, "Twobed", 2);
-        Room deluxe = new(3500, 2, 55, "Deluxe suite", 4);
-        Room pentHouse = new(6400, 4, 150, "Penthouse suite", 6);
-
-        fakeRooms.Add(oneBed);
-        fakeRooms.Add(twoBed);
-        fakeRooms.Add(deluxe);
-        fakeRooms.Add(pentHouse);
+        TestRooms(fakeRooms);
+        TestCustomers();
 
         foreach (Room room in fakeRooms)
         {
             Console.WriteLine(room);
         }
 
+        //SearchForCustomer(Connector.Connect());
+    }
 
+    private static void TestCustomers()
+    {
         List<Customer> fakeCustomer = new();
         fakeCustomer.Add(new Customer("Johan", "johan@exempel.com", "0700102030"));
         fakeCustomer.Add(new Customer("Emelie", "emelie@exempel.com", "0710203040"));
@@ -31,8 +27,18 @@ internal class Program
         {
             Console.WriteLine(customer);
         }
-        //SearchForCustomer(Connector.Connect());
     }
 
+    private static void TestRooms(List<Room> fakeRooms)
+    {
+        Room oneBed = new(840, 1, 12, "oneBed", 1);
+        Room twoBed = new(1000, 1, 24, "Twobed", 2);
+        Room deluxe = new(3500, 2, 55, "Deluxe suite", 4);
+        Room pentHouse = new(6400, 4, 150, "Penthouse suite", 6);
 
+        fakeRooms.Add(oneBed);
+        fakeRooms.Add(twoBed);
+        fakeRooms.Add(deluxe);
+        fakeRooms.Add(pentHouse);
+    }
 }
